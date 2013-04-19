3323300be2c3e66c9c56087c5d92dd3a84c4cde5
magahsc <u201010553@upc.edu.pe>
2013-04-19 02:29:49 -0400

    Registrar Personal Proyectado, Filtrado de Areas ya no repite Areas

--- ModuloPresupuesto2/GNRS.ModuloPresupuesto.UI/RegistrarPresupuestoPersonalProyectado.aspx.designer.cs
+++ ModuloPresupuesto2/GNRS.ModuloPresupuesto.UI/RegistrarPresupuestoPersonalProyectado.aspx.designer.cs
@@ -101,14 +101,5 @@ namespace GNRS.ModuloPresupuesto.UI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
-        
-        /// <summary>
-        /// RegistrarButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button RegistrarButton;
     }
 }
