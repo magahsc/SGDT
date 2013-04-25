062165ac6dffa5521c651b2c4da00434dfef8a63
JSiancas <javier.siancas@gmail.com>
2013-04-25 15:33:25 -0500

    Se eliminaron las clases Class1.cs de los proyectos.

--- ModuloPresupuesto2/GNRS.ModuloPresupuesto.UI/RegistrarPresupuestoCapacitacion.aspx.cs
+++ ModuloPresupuesto2/GNRS.ModuloPresupuesto.UI/RegistrarPresupuestoCapacitacion.aspx.cs
@@ -17,6 +17,7 @@ namespace GNRS.ModuloPresupuesto.UI
         CapacitarProyectadoBC objcapacitar = new CapacitarProyectadoBC();
         public double j ;
         public int contador;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             CursoDropDownList.Enabled = false;
